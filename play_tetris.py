362b55d4a0d1c977e517e20fe9b85dbb186d85c9
Jon <jonathan.p.lamar@gmail.com>
2021-10-01 08:39:59 -0600

    First commit with code.  Just messing aronud and scaffolding high level.

---
+++ play_tetris.py
@@ -0,0 +1,35 @@
+from time import sleep
+from pynput.keyboard import Key, Listener
+from game import GameState, KeyPress
+
+
+def onPress(key: Key) -> None:
+    global KEYPRESS
+    if key == Key.up:
+        KEYPRESS = KeyPress.UP
+    elif key == Key.down:
+        KEYPRESS = KeyPress.DOWN
+    elif key == Key.left:
+        KEYPRESS = KeyPress.LEFT
+    elif key == Key.right:
+        KEYPRESS = KeyPress.RIGHT
+    else:
+        KEYPRESS = KeyPress.NONE
+
+
+def mainLoop(game):
+    global KEYPRESS
+    KEYPRESS = KeyPress.NONE
+
+    while not game.dead:
+        game.update(KEYPRESS)
+        KEYPRESS = KeyPress.NONE
+        game.draw()
+        sleep(0.15)
+
+
+if __name__ == "__main__":
+    with Listener(on_press=onPress) as listener:
+        game = GameState()
+        mainLoop(game)
+    print("You lose!")
---
+++ utils/__init__.py
@@ -0,0 +1,89 @@
+from __future__ import annotations
+from enum import Enum
+from config import BOARD_SIZE
+import numpy as np
+
+MIDDLE_COL = BOARD_SIZE[1] // 2
+
+ROTATE_MATRIX = np.array([[0, -1], [1, 0]])
+
+
+class KeyPress(Enum):
+    UP = 1
+    DOWN = 2
+    LEFT = 3
+    RIGHT = 4
+    NONE = 5
+
+
+class Tetramino:
+    def __init__(self, squares: np.ndarray) -> None:
+        self.squares = squares
+
+    # FIXME: Still shifts squares around
+    def rotate(self) -> None:
+        shift = self.squares.min(axis=0)
+        self.squares = np.dot(self.squares - shift, ROTATE_MATRIX) + shift
+
+    def move(self, direction: KeyPress) -> None:
+        if direction == KeyPress.DOWN:
+            self.squares[:, 0] += 1
+        elif direction == KeyPress.LEFT:
+            self.squares[:, 1] -= 1
+        elif direction == KeyPress.RIGHT:
+            self.squares[:, 1] += 1
+
+
+class Eye(Tetramino):
+    def __init__(self) -> None:
+        super().__init__(
+            np.array(
+                [(0, MIDDLE_COL), (1, MIDDLE_COL), (2, MIDDLE_COL), (3, MIDDLE_COL)]
+            )
+        )
+
+
+class Ell(Tetramino):
+    def __init__(self) -> None:
+        super().__init__(
+            np.array(
+                [(0, MIDDLE_COL), (1, MIDDLE_COL), (2, MIDDLE_COL), (2, MIDDLE_COL + 1)]
+            )
+        )
+
+
+class Square(Tetramino):
+    def __init__(self) -> None:
+        super().__init__(
+            np.array(
+                [
+                    (0, MIDDLE_COL),
+                    (0, MIDDLE_COL + 1),
+                    (1, MIDDLE_COL),
+                    (1, MIDDLE_COL + 1),
+                ]
+            )
+        )
+
+
+class Tee(Tetramino):
+    def __init__(self) -> None:
+        super().__init__(
+            np.array(
+                [(0, MIDDLE_COL), (1, MIDDLE_COL), (1, MIDDLE_COL + 1), (2, MIDDLE_COL)]
+            )
+        )
+
+
+class Zee(Tetramino):
+    def __init__(self) -> None:
+        super().__init__(
+            np.array(
+                [
+                    (0, MIDDLE_COL),
+                    (1, MIDDLE_COL),
+                    (1, MIDDLE_COL + 1),
+                    (2, MIDDLE_COL + 1),
+                ]
+            )
+        )
